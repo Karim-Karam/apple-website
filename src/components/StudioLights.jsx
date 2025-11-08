fdb0750bea99435da976f4007f1189763bf7a86f
Karim-Karam <karamkarim454@gmail.com>
2025-11-08 22:03:05 +0200

    fix

--- src/components/StudioLights.jsx
+++ src/components/StudioLights.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Environment, Lightformer, spotLight } from '@react-three/drei'
+// 'spotLight' (lowercase) is removed from this import
+import { Environment, Lightformer } from '@react-three/drei'
+
 const StudioLights = () => {
     return (
         <group name='lights'>
@@ -11,31 +13,33 @@ const StudioLights = () => {
                         position={[10, 5, -5]}
                         scale={10}
                     />
-
                     <Lightformer
                         form='rect'
                         intensity={10}
                         position={[10, 0, 1]}
                         scale={10}
                     />
-
                 </group>
             </Environment>
+
+            {/* Prop typo fixed: angel -> angle */}
             <spotLight
                 position={[-2, 10, 5]}
-                angel={0.3}
+                angle={0.3}
                 decay={0}
                 intensity={Math.PI * 0.2}
             />
+            {/* Prop typo fixed: angel -> angle */}
             <spotLight
                 position={[0, -25, 10]}
-                angel={0.3}
+                angle={0.3}
                 decay={0}
                 intensity={Math.PI * 0.2}
             />
+            {/* Prop typo fixed: angel -> angle */}
             <spotLight
                 position={[0, 15, 5]}
-                angel={0.3}
+                angle={0.3}
                 decay={0.1}
                 intensity={Math.PI * 3}
             />
